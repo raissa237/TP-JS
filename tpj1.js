dabd35e6af12c21026fae394682bb9336372bf3e
raissa237 <floretiomela@gmail.com>
2021-07-26 21:28:20 +0200

    afficher et cacher le mot de passe

--- tpj1.js
+++ tpj1.js
@@ -1,14 +1,14 @@
 //1.a-changez le input "text" en input "password"
-document.querySelector("input").type = "password";
+document.getElementById("username").type = "password";
 
 
 //1.b-changez le input "password" en input "text"
-document.querySelector("input").type = "text";
+document.getElementById("password").type = "text";
 
 //Question 2
 document.querySelector(".warning").style.color = "red";
-document.querySelector("a").style.color = "yellow";
-document.querySelector("a").style.textDecoration = "none";
+document.querySelector(".warning>a").style.color = "yellow";
+document.querySelector(".warning>a").style.textDecoration = "none";
 
 //Question 3
 document.getElementById("bloc2").innerHTML = "Nouveau contenu pour ce bloc";
@@ -18,3 +18,23 @@ var inserer = document.createElement("p");
 var signature = document.createTextNode("TIOMELA Raissa");
 inserer.appendChild(signature);
 document.getElementById("global").appendChild(inserer);
+
+//Show or hide the password
+var eye = document.getElementById("eye");
+var pass = document.getElementById("password");
+var aspect = false;
+
+function changer(){
+    if(aspect){
+        pass.setAttribute("type", "password");
+        eye.style.color = "#7a797e";
+        aspect = false;
+    }
+    else{
+        pass.setAttribute("type", "text");
+        eye.style.color = "#5887ef";
+        aspect = true;
+    }
+}
+
+eye.addEventListener("click", changer);
